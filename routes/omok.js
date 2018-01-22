6bb005e13ebafce16c7bf657c19be75a63e65143
kdh <rlaeogma12@naver.com>
2018-01-22 10:41:57 +0900

    what is wrong...

--- routes/omok.js
+++ routes/omok.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 /* GET omok page */
 
+var roomNumber = 5;
+
 router.get('/', function(req, res, next) {
-  res.render('omok', { title: "KDH's Blog" });
+  res.render('omok', {
+    title: "KDH's Blog",
+    length: roomNumber
+  });
 });
+
+
 
 
 
